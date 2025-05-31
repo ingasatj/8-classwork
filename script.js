9515c5f45ee1f0bab8809671e2d2ef899ec45f5a
ingasatj <inga.sarj@gmail.com>
2025-05-31 23:53:32 +0400

    improvement

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let birthday = 1973;
 let currentYear = 2025;
 
-console.log = currentYear - birthday;
+console.log(currentYear - birthday);
+
+//  #2
+let ticketPrice = 5;
+let age = Number(prompt("ჩაწერეთ თქვენი ასაკი"));
+let film3d = prompt("ჩაწერეთ რომელი მეთოდით გინდათ ფილმის ნახავა D3 | REGULAR");
+
+if (age < 10) {
+  console.log(ticketPrice);
+} else if (age < 10 && film3d === D3) {
+  console.log(ticketPrice + 5);
+} else if (age < 65) {
+  console.log(ticketPrice + 5);
+} else if (age >= 65) {
+  console.log(ticketPrice + 10);
+} else if (age < 10 && film3d === 1) {
+  console.log(ticketPrice + 5);
+} else {
+  ("გთხოვთ ასაკი ჩაწეროთ ციფრებით");
+}

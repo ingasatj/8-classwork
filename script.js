56d89724123365c2167042261975ccbbdca8b53b
ingasatj <inga.sarj@gmail.com>
2025-06-04 14:08:43 +0400

    improvement

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
+// #1
+
 let birthday = 1973;
 let currentYear = 2025;
 
 console.log(currentYear - birthday);
 
 //  #2
-let ticketPrice = 5;
-let age = Number(prompt("1. Enter your age: "));
-let film3d = prompt("2. Enter movie type (regular or 3D):");
-let timeOfDay = prompt("3. Enter time of day (matinee or evening):");
+// შეკითხვები prompt-ში
+let age = Number(prompt("1.Enter your age:"));
+let movieType = prompt("2.Enter movie type (regular or 3D):");
+let timeOfDay = prompt("3.Enter time of day (matinee or evening):");
+
+// ბილეთის ფასი ასაკის მიხედვით
+let basePrice = 0;
 
 if (age < 10) {
-  console.log("ticket-ის ფასი იქნება 5 ლარი");
-} else if (age < 65) {
-  console.log("ticket-ის ფასი იქნება 15 ლარი");
-} else if (age >= 65) {
-  console.log("ticket-ის ფასი იქნება 10 ლარი");
+  basePrice = 5;
+} else if (age <= 65) {
+  basePrice = 15;
 } else {
-  ("გთხოვთ ასაკი ჩაწეროთ ციფრებით");
+  basePrice = 10;
 }
 
-// sds
-let x = 8;
-let y = 9;
-let mama = Number("ათი");
-
-if (x >= 8) {
-  console.log(x + y);
-} else {
-  sasa;
+// 3D-ის დამატება
+if (movieType === "3d") {
+  basePrice += 5;
 }
+
+// საღამოს სეანსის დამატება
+if (timeOfDay === "evening") {
+  basePrice += 5;
+}
+
+// Bonus: ფასდაკლების ბილეთი
+let hasDiscount = prompt("Do you have a discount ticket? (yes or no):");
+
+if (hasDiscount === "yes") {
+  // არჩევითია ან 25%-ით ფასდაკლება ან 5 ლარით (ამაზე ვიწვალე :( )
+  let discountType = prompt(
+    "Do you want 25% discount or 5 GEL off? (type '25%' or '5'):"
+  );
+
+  if (discountType === "25%") {
+    basePrice = basePrice * 0.75;
+  } else if (discountType === "5") {
+    basePrice -= 5;
+  }
+}
+// console.log-ში შედეგის გამოტანა
+console.log("ბილეთის ფასი თქვენთვის არის: " + basePrice + " ლარი.");
+
+// #3
